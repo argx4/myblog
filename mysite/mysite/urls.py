afbc27dba54a261d54c730aea0c72ef70f2d11dc
Denys Struts <denis.struts@gmail.com>
2026-09-19 18:14:42 +0300

    Лаб3

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin # type: ignore
 from django.urls import include, path # type: ignore
 
+from django.conf import settings # type: ignore
+from django.conf.urls.static import static # type: ignore
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
